0536e2cea634edf60767e31f4ec467da92c1dc4f
neosrl <horj90@naver.com>
2024-02-18 01:37:40 +0900

    add input BAR

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,40 @@
 import { useState } from "react";
 import "./App.css";
+import { Todo, createTodo } from "./apis/crud";
 
 function App() {
-  // input에 들어갈 데이터 useState
-  const [text, useText] = useState("");
+  const [todo, setTodo] = useState<Todo[]>([]);
 
+  // input에 들어갈 데이터 useState
+  const [text, setText] = useState<string>("");
+  console.log(text);
+
+  // 타이핑값 다루는 함수
   const handleChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value;
+    setText(inputValue);
   };
 
-  const handleClickButton = () => {};
+  // 요청하는 함수 ""일때 리턴 , 반환값 트루 and 타입 object 시 렌더링되도록 set
+  // create data는 객체.. 기존배열에서 전개해서 넣어줘야됨
+  const requestHandle = async () => {
+    if (text === "") return;
+    const createdData = await createTodo({ title: text });
+    if (createdData && typeof createdData === "object")
+      setTodo((beforetoDos) => [...beforetoDos, createdData]);
+    setText("");
+  };
 
-  const handleEnter = () => {};
+  // 클릭시 요청 되는것. 클릭하고 요청함수 실행 리렌더링되나 실험
+  const handleClickButton = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    requestHandle();
+  };
+
+  // 엔터키 입력시 요청되는것 엔터키 이외 리턴,
+  const handleEnter = async (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key !== "enter") return;
+  };
 
   return (
     // 줄별로 div에 담기. 바디 안에 H2
@@ -30,12 +53,19 @@ function App() {
           onChange={handleChangeValue}
           onKeyDown={handleEnter}
         ></input>
-        <button onClick={}></button>
+        <button onClick={handleClickButton}>등록</button>
       </div>
       {/* List 박스 줄 투두리스트와 Done리스트 */}
       <div>
-        <div></div>
-        <div></div>
+        {/* 박스내용 넣기 li ul  */}
+        <div>
+          <li className="toDoList">
+            <ul></ul>
+          </li>
+          <li className="doneList">
+            <ul></ul>
+          </li>
+        </div>
       </div>
     </body>
   );
