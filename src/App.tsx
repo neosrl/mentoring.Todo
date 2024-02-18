bfed6b60421291b95697132ff634932f879a4560
neosrl <horj90@naver.com>
2024-02-18 22:08:56 +0900

    save3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { Todo, createTodo } from "./apis/crud";
+import { Todo, createTodo, editTodo, getTodoList } from "./apis/crud";
+import React from "react";
+import Modal from "react-modal";
 
 function App() {
   const [todoList, setTodoList] = useState<Todo[]>([]);
 
-  // input에 들어갈 데이터 useState
+  // 모달상태관리 값, 함수들 모달에 띄울 toDo 할당
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modal, setModal] = useState({
+    id: "",
+    title: "",
+    done: false,
+  });
+  // inputbar에 들어갈 데이터 useState
   const [text, setText] = useState<string>("");
   console.log(text);
+
+  // 수정창 useState
+  const [areaText, setAreaText] = useState(modal.title);
+
+  // 화면 렌더링 get 함수 useEffect
+  useEffect(() => {
+    async () => {
+      const resData = await getTodoList();
+      setTodoList(resData);
+    };
+  }, []);
 
   // 타이핑값 다루는 함수
   const handleChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -34,22 +54,64 @@ function App() {
   // 엔터키 입력시 요청되는것 엔터키 이외 리턴,
   const handleEnter = async (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key !== "enter") return;
+    requestHandle();
   };
 
-  // 수정하기 버튼 이벤트 함수
-  //
-  const handleClickEdit = () => {};
-  // 삭제하기 버튼 이벤트 함수
-  const handleClickRemove = () => {};
+  //수정 요청하기
+
+  const requestEdit = async () => {
+    const resData = await editTodo(modal.id, {
+      title: areaText,
+      done: modal.done,
+    });
+
+    if (resData && typeof resData === "object") console.log(resData);
+    // setTodoList(res);
+    setModalIsOpen(false);
+    setModal({ id: "", title: "", done: false || true });
+  };
+
+  // 수정하기 버튼 클릭 이벤트 함수
+  const handleSubmitEdit = () => {
+    if (!areaText) return;
+    requestEdit();
+  };
+
+  //  삭제하기 버튼 이벤트 함수
+  // const handleClickRemove = () => {};
+
+  // 날짜변환 -> Fail...
+  // console.log(todoList[0].createdAt);
+  // const tranferDate = todoList.map((list) => list.createdAt).toLocaleString;
+
+  // 모달창 구현하기
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  // 모달 취소버튼 이벤트 함수
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+  // 모달창 input text 이벤트 함수
+  const handleModalText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const modalText = e.target.value;
+    setAreaText(modalText);
+  };
+
+  // const handleClickTodo = () => {
+  //   const findID = find();
+  //   setModalOpen();
+  // };
 
   return (
     // 줄별로 div에 담기. 바디 안에 H2
 
-    <body>
+    <div>
       <div>
         <h2>ToDo List!</h2>
       </div>
-
       {/* input 박스 줄 인풋에 useState 값 value에 연결*/}
       {/* 이벤트 함수 추가 */}
       <div>
@@ -65,23 +127,47 @@ function App() {
       <div>
         {/* 박스내용 넣기 li ul map함수로 toDo 배열생성 */}
         {/* 수정 삭제 버튼*/}
-        <div>
-          <li className="toDoList">
-            {todoList.map((todo) => (
+        <li className="toDoList">
+          {todoList.map((todo) => (
+            // Todo wrapper display flex
+            // Todo 고유key값 안넣으면 map돌릴때 child 고유성없다고 에러
+
+            <div key={todo.id}>
+              <div>{todo.title}</div>
+              <div>{todo.createdAt}</div>
+              <button
+                onClick={() => {
+                  setModal({ id: todo.id, title: todo.title, done: todo.done });
+                  openModal();
+                }}
+              >
+                수정하기
+              </button>
               <div>
-                <div>{todo.title}</div>
-                <div>
-                  {/* 버튼 이벤트 추가하기 */}
-                  <button>생성일자</button>
-                  <button onClick={handleClickEdit}>수정하기</button>
-                  <button onClick={handleClickRemove}>삭제하기</button>
-                </div>
+                <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+                  <h2>모달 제목</h2>
+                  <form onSubmit={handleSubmitEdit}>
+                    <input
+                      value={areaText}
+                      defaultValue={modal.title}
+                      onChange={handleModalText}
+                    />
+
+                    <button type="submit">수정</button>
+                  </form>
+                </Modal>
               </div>
-            ))}
-          </li>
-        </div>
+              {/* <button onClick={handleClickRemove}>삭제하기</button> */}
+            </div>
+          ))}
+        </li>
       </div>
-    </body>
+      {/* 모달창 어떻게 구현할지.... */}
+      {/* 클릭한 toDo의 ID값 지정해서 연결? */}
+      {/* 버튼 이벤트 추가하기 */}
+
+      {/* {modal.id && typeof modal === "object" ? <ModalCompo /> : null} */}
+    </div>
   );
 }
 
